3d7a58063f40a427eefec2c63fd5f6f44f28ed93
frozenburger <jenova195@gmail.com>
2020-09-05 10:39:13 +0900

    data preprocessing complete

--- data_100k/fetch.py
+++ data_100k/fetch.py
@@ -1,63 +1,30 @@
-import pandas as pd
-
-
-
 # preprocessing
 import pandas as pd
 import numpy as np
 
 def movielens():
-    movies = pd.read_csv('data/movies.csv')
-    ratings = pd.read_csv('data/ratings.csv')
+    ratings_data = pd.read_csv('data_100k/u.data', sep='\t', header=None).drop(columns=3)
+    ratings_data = ratings_data.pivot(index=0, columns=1, values=2).fillna(0)
+    ratings_data.values[4 <= ratings_data.values] = 0
+    ratings_data.values[ratings_data.values <= 3] = 1
+    interaction_matrix = ratings_data.to_numpy()
 
-    genre_list = []
-    for index, row in movies.iterrows():
-        for item in row.genres.split('|'):
-            if item not in genre_list:
-                genre_list.append(item)
-
+    user_data = pd.read_csv('data_100k/u.user', header=None)
+    job_list = pd.read_csv('data_100k/u.occupation', header=None)
     array = []
-    for iteration in range(len(movies)):
-        current_movie_genres = movies.loc[iteration].genres.split('|')
-        embedding = list(map(lambda x : 1 if x in current_movie_genres else 0, genre_list))
+    for iteration in range(len(user_data)):
+        current_user_job = user_data.iloc[iteration].values.item().split('|')[3]
+        embedding = list(map(lambda x: 1 if x == current_user_job else 0, job_list[0]))
         array.append(embedding)
+    user_feat_indicators = np.array(array)
+
+    movies_data = pd.read_csv('data_100k/u.item', sep='|', encoding='latin-1', header=None)
+    genre_df = pd.read_csv('data_100k/u.genre', header=None)[0]
+    genre_list = list(map(lambda x: x.split('|')[1], genre_df.to_list()))
+    array = []
+    for iteration in range(len(movies_data)):
+        current_movie_genres = movies_data.loc[iteration][5:].to_list()
+        array.append(current_movie_genres)
     item_feat_indicators = np.array(array)
-
-    ratings = ratings.drop(columns = ['timestamp'])
-    missing_list = []
-    movie_lst = ratings.movieId.unique()
-    for id in movies.movieId:
-        if int(id) not in movie_lst:
-            missing_list.append(id)
-    for id in missing_list:
-        ratings = ratings.append({'userId':1, 'movieId':int(id), 'rating':0.0}, ignore_index=True)
-    ratings = ratings.pivot(index='userId', columns='movieId', values='rating').fillna(0)
-    ratings.values[4 <= ratings.values] = 0
-    ratings.values[ratings.values <= 3] = 1
-    interaction_matrix = ratings
-
-    user_data = pd.read_csv('u.user', header=None)
-    job_list = pd.read_csv('u.occupation', header=None)
-    array = []
-    for iteration in range(len(user_data)):
-        current_user_job = user_data.iloc[iteration].values.item().split('|')[3]
-        embedding = list(map(lambda x: 1 if x==current_user_job else 0, job_list))
-        array.append(embedding)
-    user_feat_indicators = np.array(array)
-
-    user_data = pd.read_csv('u.user')
-    job_list = pd.read_csv('u.occupation', header=None)
-    job_list = []
-    for index, row in user_data.iterrows():
-        value = user_data.iloc[index].values.item()
-        occ = value.split('|')[3]
-        if occ not in job_list:
-            job_list.append(occ)
-    array = []
-    for iteration in range(len(user_data)):
-        current_user_job = user_data.iloc[iteration].values.item().split('|')[3]
-        embedding = list(map(lambda x: 1 if x == current_user_job else 0, job_list))
-        array.append(embedding)
-    user_feat_indicators = np.array(array)
 
     return interaction_matrix, user_feat_indicators, item_feat_indicators
--- tf_lightfm.py
+++ tf_lightfm.py
@@ -13,10 +13,10 @@ class model():
         self.interactions = tf.constant(interactions, dtype=tf.float32) #유저x상품 상호작용이 positive 시 0, negative 시 1
         self.user_feat_indicators = tf.Variable(user_feat_indicators, dtype=tf.float32) #유저피쳐 해당시 1, 아닐시 0 ??
         self.item_feat_indicators = tf.Variable(item_feat_indicators, dtype=tf.float32) #상품피쳐 해당시 1, 아닐시 0
-        self.user_feat_embeddings = tf.Variable(init_xav(self.size_uf, self.k_embeddings), dtype=tf.float32) #학습시킬 각 유저피쳐 임베딩
-        self.item_feat_embeddings = tf.Variable(init_xav(self.size_if, self.k_embeddings), dtype=tf.float32) #학습시킬 각 상품피쳐 임베딩
-        self.user_bias_vector = tf.Variable(init_xav(self.size_u), dtype=tf.float32) # ??
-        self.item_bias_vector = tf.Variable(init_xav(self.size_i), dtype=tf.float32) # ??
+        self.user_feat_embeddings = tf.Variable(init_xav(shape=(self.size_uf, self.k_embeddings)), dtype=tf.float32) #학습시킬 각 유저피쳐 임베딩
+        self.item_feat_embeddings = tf.Variable(init_xav(shape=(self.size_if, self.k_embeddings)), dtype=tf.float32) #학습시킬 각 상품피쳐 임베딩
+        self.user_bias_vector = tf.Variable(init_xav(shape=(self.size_u,)), dtype=tf.float32) # ??
+        self.item_bias_vector = tf.Variable(init_xav(shape=(self.size_i,)), dtype=tf.float32) # ??
 
 
     def get_prediction_matrix(self):
@@ -33,4 +33,5 @@ class model():
     def train(self, epoch=30):
         opt = tf.keras.optimizers.Adagrad(learning_rate=0.05)
         for i in range(epoch):
+            print('epoch : {}, loss : {}'.format(i, self.objective_function()))
             opt.minimize(self.objective_function, var_list=[self.user_feat_embeddings, self.item_feat_embeddings])

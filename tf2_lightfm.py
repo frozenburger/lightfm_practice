d380081f65577c7350bf0a19730991d55d48582e
frozenburger <frozenburger@gmail.com>
2020-09-10 20:14:36 +0900

    debugging : learning rate does not change

--- tf2_lightfm.py
+++ tf2_lightfm.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import numpy as np
+import pdb
 
 class model():
     def __init__(self, interactions, user_feat_indicators, item_feat_indicators, labels, test_data, k_embeddings=10):
@@ -13,12 +14,18 @@ class model():
         self.k_embeddings = k_embeddings
 
         self.interactions = tf.constant(interactions, dtype=tf.float32) #유저x상품 상호작용이 positive 시 0, negative 시 1
+        self.positives = tf.constant((interactions == 1).astype(float), dtype=tf.float32)
+        self.negatives = tf.constant((interactions == 0).astype(float), dtype=tf.float32)
+        self.true_matrix = self.positives + self.negatives
+
         self.user_feat_indicators = tf.constant(user_feat_indicators, dtype=tf.float32) #유저피쳐 해당시 1, 아닐시 0 ??
         self.item_feat_indicators = tf.constant(item_feat_indicators, dtype=tf.float32) #상품피쳐 해당시 1, 아닐시 0
         self.user_feat_embeddings = tf.Variable(init_xav(shape=(self.size_uf, self.k_embeddings)), dtype=tf.float32) #학습시킬 각 유저피쳐 임베딩
         self.item_feat_embeddings = tf.Variable(init_xav(shape=(self.size_if, self.k_embeddings)), dtype=tf.float32) #학습시킬 각 상품피쳐 임베딩
         self.user_feat_bias_vector = tf.Variable(init_xav(shape=(self.size_uf,)), dtype=tf.float32) # ??
         self.item_feat_bias_vector = tf.Variable(init_xav(shape=(self.size_if,)), dtype=tf.float32) # ??
+
+        self.opt = tf.keras.optimizers.Adagrad(learning_rate=0.05)
 
     def get_prediction_matrix(self):
         return tf.sigmoid(
@@ -42,11 +49,37 @@ class model():
         squared_diff_relevant = tf.where(mask, tf.zeros_like(squared_diff), squared_diff)
         return tf.sqrt(tf.reduce_mean(squared_diff_relevant))
 
+    def linear_loss(self):
+        positive_scores = tf.multiply(self.get_prediction_matrix(), self.positives)
+        negative_scores = self.negatives - tf.multiply(self.get_prediction_matrix(), self.negatives)
+        squared_diff = tf.math.squared_difference(self.true_matrix, positive_scores+negative_scores)
+        return tf.sqrt(tf.reduce_mean(squared_diff))
+
+
     def train(self, epoch=30):
         opt = tf.keras.optimizers.Adagrad(learning_rate=0.05)
         for i in range(epoch):
             print('epoch : {}, loss : {}'.format(i, self.rms_loss()))
             opt.minimize(self.rms_loss, var_list=[self.user_feat_embeddings, self.item_feat_embeddings, self.user_feat_bias_vector, self.item_feat_bias_vector])
+
+
+    def train_manually(self, epoch=30):
+        opt = self.opt
+        for i in range(epoch):
+            with tf.GradientTape() as tape:
+                def loss():
+                    positive_scores = tf.multiply(self.get_prediction_matrix(), self.positives)
+                    negative_scores = self.negatives - tf.multiply(self.get_prediction_matrix(), self.negatives)
+                    squared_diff = tf.math.squared_difference(self.true_matrix, positive_scores + negative_scores)
+                    return tf.sqrt(tf.reduce_mean(squared_diff))
+                gradients = tape.gradient(loss(), [self.user_feat_embeddings, self.item_feat_embeddings, self.user_feat_bias_vector, self.item_feat_bias_vector])
+                opt.apply_gradients(zip(gradients, [self.user_feat_embeddings, self.item_feat_embeddings, self.user_feat_bias_vector, self.item_feat_bias_vector]))
+                print('loss : {}, learning rate : {}'.format(loss().numpy(), opt.get_config()['learning_rate']))
+
+                #print(list(map(lambda x : np.reciprocal(x.numpy()).astype(float), gradients))) # (21, 10) / (19, 10) / (21, ) / (19, )
+
+    def get_optimizer(self):
+        return self.opt
 
     def predict(self, user_id):
         user_pred = self.get_prediction_matrix().__array__()[user_id]
@@ -62,3 +95,12 @@ class model():
         for movie_id in unwatched_scores:
             print('movie : {}, score : {}, actual value : {}'.format(self.labels[movie_id], user_pred[movie_id], self.test_data[user_id][movie_id]))
 
+
+'''
+# execution shortcut
+from data_100k.fetch import movielens_split
+from tf2_lightfm import model
+d1, d2, d3, d4, d5 = movielens_split()
+cls = model(d1, d2, d3, d4, d5)
+cls.train_manually(1)
+'''
